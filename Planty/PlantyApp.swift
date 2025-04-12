2ea338292a0a19b2292add017dfad4d6e0e9d082
julie <julie.ryan.france@gmail.com>
2025-04-12 21:44:55 +0200

    Initial Commit

---
+++ Planty/PlantyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantyApp.swift
+//  Planty
+//
+//  Created by julie ryan on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlantyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
